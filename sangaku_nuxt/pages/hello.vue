46d3b427bf187536edb08036300a90fc79040290
kitadesega <segarian8@gmail.com>
2019-06-14 11:54:49 +0900

    init

---
+++ sangaku_nuxt/pages/hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+
+<script>
+  export default {
+    async asyncData ({ app }) {
+      const data = await app.$axios.$get('http://localhost:8000/api')
+      return { data }
+    }
+  }
+
+</script>
